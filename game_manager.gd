c77ac076f0400bce93d454a5a27878011f4b736f
stoklosaszymon <stoklosa.szymon@gmail.com>
2025-06-25 15:26:28 +0200

    simulate light and shadow

--- game_manager.gd
+++ game_manager.gd
@@ -2,3 +2,4 @@ extends Node
 
 var player : Node = null
 var previous_scene_path = "res://main.tscn"
+var directional_light: DirectionalLight2D = null;
---
+++ sun.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+@onready var sun: DirectionalLight2D = $DirectionalLight2D
+
+const START_ANGLE := -60.0  # sunrise
+const END_ANGLE := 240.0    # sunset
+
+@export var day_length: float = 30.0  # seconds for a full cycle
+
+var time_passed := 0.0
+
+func _process(delta):
+	time_passed += delta
+
+	var t = fmod(time_passed, day_length) / day_length
+
+	var angle_deg = lerp(START_ANGLE, END_ANGLE, t)
+	sun.rotation_degrees = angle_deg
+	sun.height = lerp(0.2, 0.9, t)
+	
+	var brightness = clamp(sin(deg_to_rad(angle_deg)), 0.2, 0.8)
+	sun.energy = brightness
