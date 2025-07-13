9bf57f31a448448a821d8951298965e76003855e
stoklosaszymon <stoklosa.szymon@gmail.com>
2025-07-13 22:59:13 +0200

    add gate, invisible colision block

--- cave.gd
+++ cave.gd
@@ -2,7 +2,7 @@ extends Node
 
 
 func _on_wall_detector_area_entered(area: Area2D) -> void:
-	area.get_parent().z_index = 2
+	area.get_parent().z_index = 4
 
 
 func _on_wall_detector_area_exited(area: Area2D) -> void:
--- door.gd
+++ door.gd
@@ -11,12 +11,3 @@ func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) 
 		else: 
 			$AnimatedSprite2D.play("close")
 			$CollisionPolygon2D.set_deferred("disabled", false)
-
-func _on_area_2d_2_area_entered(area: Area2D) -> void:
-	print("enterd")
-	z_index = 3
-
-
-func _on_area_2d_2_area_exited(area: Area2D) -> void:
-	print("exited")
-	z_index = 5
