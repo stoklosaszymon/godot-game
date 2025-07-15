8beec14e26305792d060946210f6f7c351ea79ef
stoklosaszymon <stoklosa.szymon@gmail.com>
2025-07-15 19:27:10 +0200

    fix unequip item

--- grid_container.gd
+++ grid_container.gd
@@ -5,26 +5,24 @@ func _can_drop_data(at_position: Vector2, data: Variant) -> bool:
 	
 func _drop_data(at_position: Vector2, data: Variant) -> void:
 	var dragged_item = data.item_data
-	
 	if data.source_slot_node in get_children():
 		return 
-		
-	if get_parent().get_parent().name == "TargetInventoryGrid":
+
+	if data.source_slot_node.name == "UseSlot":
+		data.source_slot_node.clear()
+		GameManager.player.unequip()
+	elif get_parent().get_parent().name == "TargetInventoryGrid":
 		if GameManager.target_inventory_id != null:
 			GameManager.add_item_to_target(dragged_item, GameManager.chests_data[GameManager.target_inventory_id] as Array[ItemData])
 			GameManager.remove_item_from_target(dragged_item, PlayerState.inventory)
 		
-	if get_parent().get_parent().name == "InventoryGrid":
+	elif get_parent().get_parent().name == "InventoryGrid":
 		GameManager.add_item_to_target(dragged_item, PlayerState.inventory)
 		if GameManager.target_inventory_id != null:
 			GameManager.remove_item_from_target(dragged_item, GameManager.chests_data[GameManager.target_inventory_id])
-		
-	if data.source_slot_node.name == "UseSlot":
-		data.source_slot_node.clear()
-		GameManager.player.unequip()
-	
+
 	if GameManager.target_inventory != null:
 		GameManager.target_inventory.open();
-	
+
 	if GameManager.inventory != null:
 		GameManager.inventory.open();
