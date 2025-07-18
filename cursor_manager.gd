351418649718182b5bfdc1ea315b9c2bed62addd
stoklosaszymon <stoklosa.szymon@gmail.com>
2025-07-19 00:25:56 +0200

    add cursor

--- cursor_manager.gd
+++ cursor_manager.gd
@@ -3,6 +3,7 @@ extends Node
 var current_cursor = Input.CURSOR_ARROW
 var cursor_lock = preload("res://Assets/UI/cursor_lock.png")
 var cursor_openhand = preload("res://Assets/UI/cursor_openhand.png")
+var cursor_pointhand = preload("res://Assets/UI/cursor_pointhand.png")
 
 func set_cursor_lock():
 	Input.set_custom_mouse_cursor(cursor_lock)
@@ -14,4 +15,4 @@ func set_cursor_open_hand():
 	Input.set_custom_mouse_cursor(cursor_openhand)
 	
 func set_cursor_hand():
-	Input.set_custom_mouse_cursor(null, Input.CURSOR_POINTING_HAND)
+	Input.set_custom_mouse_cursor(cursor_pointhand)
