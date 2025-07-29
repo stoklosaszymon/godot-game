d81db89e2ee280f81d9090e00d7c28ec3cd33e69
stoklosaszymon <stoklosa.szymon@gmail.com>
2025-07-29 23:00:23 +0200

    add mountains

--- cave_enterance.gd
+++ cave_enterance.gd
@@ -1,11 +1,13 @@
 extends StaticBody2D
 
+var player_z = 0;
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	if area.name == "PlayerArea":
-		z_index = -1;
+		z_index = -1
+
 
 
 func _on_area_2d_area_exited(area: Area2D) -> void:
 	if area.name == "PlayerArea":
-		z_index = 3;
+		area.z_index = player_z;
