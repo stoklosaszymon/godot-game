66215c237ab26592d50f60b18112dbfd33f215e9
stoklosaszymon <stoklosa.szymon@gmail.com>
2025-08-11 18:02:28 +0200

    update rain

--- environment.gd
+++ environment.gd
@@ -3,32 +3,39 @@ extends CanvasLayer
 @onready var rain : CPUParticles2D = $Rain
 @onready var river = $"../Map/River"
 @export var day_length := 300.0
-@export var min_rain_duration := 30.0
-@export var max_rain_events := 3
+@export var min_rain_duration := 60.0
+@export var max_rain_events := 2
 
 var day_timer := 0.0
 var rain_timer := 0.0
 var rain_event_count := 0
+var min_time_between_rains = 120.0
+var was_raining_recently = false
+var time_since_last_rain = 0;
 var is_raining := false
 
 func _ready():
 	randomize()
-	_maybe_start_rain()
 
 func _process(delta):
 	day_timer += delta
+	if was_raining_recently:
+		time_since_last_rain += delta
+		
+	if time_since_last_rain > min_time_between_rains:
+		was_raining_recently = false
+		
 	if day_timer >= day_length:
 		day_timer = 0.0
 		rain_event_count = 0
-		_maybe_start_rain()
 
 	if is_raining:
 		rain_timer -= delta
 		if rain_timer <= 0.0:
 			_set_rain(false)
-			_maybe_start_rain()
 	else:
-		if rain_event_count < max_rain_events and randf() < delta / 10.0:
+		if rain_event_count < max_rain_events and randf() < delta / 10.0 and !was_raining_recently:
+			was_raining_recently = true
 			_start_rain()
 
 func _start_rain():
@@ -36,11 +43,6 @@ func _start_rain():
 	rain_event_count += 1
 	rain_timer = min_rain_duration
 	_set_rain(true)
-
-func _maybe_start_rain():
-	if not is_raining and rain_event_count < max_rain_events:
-		if randf() < 0.3:  # ~30% chance to start immediately
-			_start_rain()
 
 func _set_rain(enabled: bool):
 	is_raining = enabled
